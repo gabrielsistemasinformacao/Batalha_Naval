5dca552df0446685296c6f601c4fb87c0e21deed
Gabriel <gabrielsistemasinformacao@gmail.com>
2025-04-28 20:53:03 -0300

    Desafio Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,87 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+     char letras[10] = {'A','B','C','D','E','F','G','H','I','J'};
+     int posicaoletra = 0;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+     int tabuleiro [10] [10];
+     
+
+     for (int i = 0; i < 10; i++)
+     {
+        for (int y = 0; y < 10; y++)
+        {
+            tabuleiro[i][y] = 0;
+        }    
+    }
+
+    char letra;
+    do
+    {
+        printf("Escolha de A a J: ");
+        scanf("%c", &letra);
+    } while (!((letra >= 'A') && (letra <= 'J')));
+    int posicaonumero;
+    do
+    {
+        printf("Escolha de 1 a 10: ");
+        scanf("%i", &posicaonumero);
+    } while ((posicaonumero < 1 || posicaonumero > 10));
+
+    int posicao;
+    do
+    {
+        printf("Escolha posição: 1 para horizontal   2 para vertical");
+        scanf("%i", &posicao);
+    } while (((posicao != 1) && (posicao != 2)));
+
+
+    while (posicaoletra<10)
+    {
+        if (letras[posicaoletra] == letra)
+        {
+            break;
+        }
+        posicaoletra++;
+    }
+    
+    if (posicao == 2)
+    for (int i = posicaonumero-1; i < (posicaonumero-1)+3; i++)
+    {
+        tabuleiro[i][posicaoletra] = 3;
+    }
+    else if (posicao == 1)
+    for (int i = posicaonumero-1; i < (posicaonumero-1)+3; i++)
+    {
+        tabuleiro[posicaoletra][i] = 3;
+    }
+    
+    
+    
+    printf("     ");
+     for (int i = 0; i < 10; i++)
+     {
+        printf("%c ", letras[i]);
+     }
+     printf("\n");
+    for (int i = 0; i < 10; i++)
+     {
+        if (i == 9)
+            printf("%i   ",i+1);
+        else
+            printf("%i    ",i+1);
+            
+        for (int y = 0; y < 10; y++)
+        {
+            printf("%i ",tabuleiro[i][y]);
+        }    
+        printf("\n");
+    }
+     
+      
+
+
 
     return 0;
 }
